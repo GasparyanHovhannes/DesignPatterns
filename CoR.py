490a2410bd08491706e1156cfd4168da3932e52a
Hovhannes <hovhannesg445@gmail.com>
2025-06-18 11:40:17 +0400

    Added Builder design pattern

--- CoR.py
+++ CoR.py
@@ -9,52 +9,47 @@ class Approver(ABC):
         self.next_approver = approver
         return approver
 
-    def pass_to_next(self, amount):
-        if self.next_approver:
-            self.next_approver.handle_request(amount)
-        else:
-            print("Request cannot be approved.")
-
-    @abstractmethod
     def handle_request(self, amount):
         pass
+
 
 class TeamLeader(Approver):
     def handle_request(self, amount):
         if amount <= 1000:
-            print(f"Team Leader {self.name} approved ${amount}")
+            print(f"Team Leader {self.name} approved the request of ${amount}")
         else:
-            self.pass_to_next(amount)
+            super().handle_request(amount)
 
 
 class DepartmentHead(Approver):
     def handle_request(self, amount):
         if amount <= 10000:
-            print(f"Department Head {self.name} approved ${amount}")
+            print(f"Department Head {self.name} approved the request of ${amount}")
         else:
-            self.pass_to_next(amount)
+            super().handle_request(amount)
 
 
 class FinanceDirector(Approver):
     def handle_request(self, amount):
         if amount <= 25000:
-            print(f"Finance Director {self.name} approved ${amount}")
+            print(f"Finance Director {self.name} approved the request of ${amount}")
         else:
-            self.pass_to_next(amount)
+            super().handle_request(amount)
 
 
 class CEO(Approver):
     def handle_request(self, amount):
-        print(f"CEO {self.name} approved ${amount}")
+        print(f"CEO {self.name} approved the request of ${amount}")
 
-team_leader = TeamLeader("Alice")
+team_leader = TeamLeader("Mher")
 department_head = DepartmentHead("Bob")
-finance_director = FinanceDirector("Carol")
-ceo = CEO("Diana")
+finance_director = FinanceDirector("Malvina")
+ceo = CEO("Panayotus Dmitrius")
 
 team_leader.set_next(department_head).set_next(finance_director).set_next(ceo)
 
 team_leader.handle_request(500)
-team_leader.handle_request(12000)
+team_leader.handle_request(7500)
+team_leader.handle_request(20000)
 team_leader.handle_request(50000)
-team_leader.handle_request(999999)
+team_leader.handle_request(1000000)
